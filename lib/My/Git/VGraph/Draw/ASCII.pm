b35cf2a048c14447ed95e25a645b7b097bccf4f5
Darren Embry <dse@webonastick.com>
2023-07-25 16:51:45 -0400

    stubby mode, not accessible yet

--- lib/My/Git/VGraph/Draw/ASCII.pm
+++ lib/My/Git/VGraph/Draw/ASCII.pm
@@ -7,6 +7,8 @@ sub new {
     my ($class, %args) = @_;
     my $self = bless(\%args, $class);
     $self->{bullet} //= $self->{options}->{bullet} // "#";
+    $self->{stubby} //= $self->{options}->{stubby} // 0;
+    $self->{unicode} //= $self->{options}->{unicode} // 0;
     return $self;
 }
 sub vertical {
@@ -26,6 +28,8 @@ sub draw {
     # conceptually specifies what kind of drawing you're doing; in
     # reality only determines type of return value.
     my $mode = $args{mode};
+
+    my $stubby = $self->{stubby};
 
     my @revLists = @{$args{revLists} // []};
     my $code = scalar @revLists ? $revLists[0]{code} : undef;
@@ -48,35 +52,72 @@ sub draw {
     my @leftColumns  = grep { $_ < $diagonalsFromColumn } @diagonalsToColumns;
     my @rightColumns = grep { $_ > $diagonalsFromColumn } @diagonalsToColumns;
 
+    my $leftLow = $self->{unicode} ? "\N{LEFT LOW PARAPHRASE BRACKET}" : ".";
+    my $rightLow = $self->{unicode} ? "\N{RIGHT LOW PARAPHRASE BRACKET}" : ".";
+    my $leftRaised = $self->{unicode} ? "\N{LEFT RAISED OMISSION BRACKET}" : "'";
+    my $rightRaised = $self->{unicode} ? "\N{RIGHT RAISED OMISSION BRACKET}" : "'";
+
     if (defined $diagonalsFromColumn) {
         if (scalar @leftColumns) {
             $useLine1 = 1;
-            substr($line1, $diagonalsFromColumn * $colSep - 1, 1) = '/';
+            if ($stubby) {
+                substr($line1, $diagonalsFromColumn * $colSep - 1, 1) = $rightRaised;
+            } else {
+                substr($line1, $diagonalsFromColumn * $colSep - 1, 1) = '/';
+            }
             my $leftmostColumn = min @leftColumns;
             foreach my $column (@leftColumns) {
-                next if $column == $diagonalsFromColumn - 1 && $colSep < 3;
-                $useLine2 = 1;
-                substr($line2, $column * $colSep + 1, 1) = '/';
+                if ($stubby) {
+                    if ($column == $diagonalsFromColumn - 1 && $colSep < 3) {
+                        substr($line1, $column * $colSep + 1, 1) = "/";
+                    } else {
+                        substr($line1, $column * $colSep + 1, 1) = $rightLow;
+                    }
+                } else {
+                    next if $column == $diagonalsFromColumn - 1 && $colSep < 3;
+                    $useLine2 = 1;
+                    substr($line2, $column * $colSep + 1, 1) = '/';
+                }
             }
             my $pos1 = $leftmostColumn * $colSep + 2;
             my $pos2 = $diagonalsFromColumn * $colSep - 2;
             if ($pos1 <= $pos2) {
-                substr($line1, $pos1, $pos2 - $pos1 + 1) = '_' x ($pos2 - $pos1 + 1);
+                if ($stubby) {
+                    substr($line1, $pos1, $pos2 - $pos1 + 1) = '-' x ($pos2 - $pos1 + 1);
+                } else {
+                    substr($line1, $pos1, $pos2 - $pos1 + 1) = '_' x ($pos2 - $pos1 + 1);
+                }
             }
         }
         if (scalar @rightColumns) {
             $useLine1 = 1;
-            substr($line1, $diagonalsFromColumn * $colSep + 1, 1) = '\\';
+            if ($stubby) {
+                substr($line1, $diagonalsFromColumn * $colSep + 1, 1) = $leftRaised;
+            } else {
+                substr($line1, $diagonalsFromColumn * $colSep + 1, 1) = '\\';
+            }
             my $rightmostColumn = max @rightColumns;
             foreach my $column (@rightColumns) {
-                next if $column == $diagonalsFromColumn + 1 && $colSep < 3;
-                $useLine2 = 1;
-                substr($line2, $column * $colSep - 1, 1) = '\\';
+                if ($stubby) {
+                    if ($column == $diagonalsFromColumn + 1 && $colSep < 3) {
+                        substr($line1, $column * $colSep - 1, 1) = "\\";
+                    } else {
+                        substr($line1, $column * $colSep - 1, 1) = $leftLow;
+                    }
+                } else {
+                    next if $column == $diagonalsFromColumn + 1 && $colSep < 3;
+                    $useLine2 = 1;
+                    substr($line2, $column * $colSep - 1, 1) = '\\';
+                }
             }
             my $pos1 = $diagonalsFromColumn * $colSep + 2;
             my $pos2 = $rightmostColumn * $colSep - 2;
             if ($pos1 <= $pos2) {
-                substr($line1, $pos1, $pos2 - $pos1 + 1) = '_' x ($pos2 - $pos1 + 1);
+                if ($stubby) {
+                    substr($line1, $pos1, $pos2 - $pos1 + 1) = '-' x ($pos2 - $pos1 + 1);
+                } else {
+                    substr($line1, $pos1, $pos2 - $pos1 + 1) = '_' x ($pos2 - $pos1 + 1);
+                }
             }
         }
         if (grep { $diagonalsFromColumn == $_ } @diagonalsToColumns) {
@@ -95,8 +136,11 @@ sub draw {
     if ($self->{colSep} < 3) {
         foreach my $column (@diagonalsToColumns) {
             if ($column == $diagonalsFromColumn - 1 || $column == $diagonalsFromColumn + 1) {
+                # there is a diagonal from column n to column n-1 or
+                # n+1 if there are also diagonals to columns 2+
+                # columns away
                 if ($useLine2) {
-                    substr($line2, $column * $colSep, 1) = '!';
+                    substr($line2, $column * $colSep, 1) = '|';
                 }
             }
         }
